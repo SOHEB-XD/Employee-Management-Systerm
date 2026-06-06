69b2e990e457903b1e79a301af30305b887ca878
SOHEB... <soheb8976@gmail.com>
2026-06-06 10:56:48 +0530

    Remove unused getEmployees import from EmployeeList

--- client/src/pages/EmployeeList.jsx
+++ client/src/pages/EmployeeList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { getEmployees, deleteEmployee } from "../services/employeeService";
+import { deleteEmployee } from "../services/employeeService";
 import axios from "axios";
 
 const EmployeeList = () => {
